dfecdb35ce40df3f8a6830afd1f2d480e933e492
Josimar Silva <josimaralves.ti@gmail.com>
2026-02-19 15:55:56 +0100

    chore(release): prepare for release v0.2.0

--- cmd/smaug/version.go
+++ cmd/smaug/version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version information. Updated by release process.
-const Version = "0.2.0-SNAPSHOT"
+const Version = "0.2.0"
 
 // BuildTime is set during build via ldflags
 var BuildTime = "unknown"
